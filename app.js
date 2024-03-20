9315ae5b76e413c63f2f17d1606bdff248345f0d
Awais12xd <157825343+Awais12xd@users.noreply.github.com>
2024-03-20 10:19:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,220 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+let selecte = document.querySelectorAll(" select");
+let select1 = document.querySelector(".from- select ");
+let select2= document.querySelector(".to- select ");
+let img1 = document.querySelector(".from- img");
+let img2 = document.querySelector(".to- img");
+let buton = document.querySelector(".convert");
+let amount = document.querySelector("input");
+let show = document.querySelector(".show");
+
+
+for(part of selecte){
+  for(code in countryList){
+    let newOption = document.createElement("option");
+    newOption.innerText=code;
+    newOption.value=code;
+    if( code === "USD" && part.name === "from"  )
+    {
+     newOption.selected = "selected";
+    } else if( code === "PKR" && part.name === "to"){
+      newOption.selected = "selected";
+    }
+    part.append(newOption);
+  }
+}
+let changeFlag1 = (tag) => {
+  let newCode = tag.value;
+  let counCode = countryList[newCode];
+  let newUrl = `https://flagsapi.com/${counCode}/flat/64.png`
+  img1.src = newUrl;
+}
+let changeFlag2 = (tag) => {
+  let newCode = tag.value;
+  let counCode = countryList[newCode];
+  let newUrl = `https://flagsapi.com/${counCode}/flat/64.png`
+  img2.src = newUrl;
+}
+select1.addEventListener("change",(evt) => {
+  changeFlag1(evt.target);
+
+} );
+select2.addEventListener("change",(evt) => {
+  changeFlag2(evt.target);
+} );
+let url2 = "https://v6.exchangerate-api.com/v6/09517c0e75248a9920d28911/pair";
+
+
+buton.addEventListener( "click", async (evt) => {
+  evt.preventDefault();
+  let newUrl = `${url2}/${select1.value}/${select2.value}`;
+   let amtValue = amount.value;
+   let url1 = await fetch( `${newUrl}`);
+  let response = await url1.json();
+  let rati = response.conversion_rate;
+  let newValue = rati * amtValue;
+  show.innerText = `${amtValue}${select1.value} = ${newValue}${select2.value}`;
+} );
+
+
+
